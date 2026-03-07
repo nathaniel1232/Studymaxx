b03b7a28bc31a2ac7b5159c45064844eb74a45fd
edu8042119 <edu8042119@agderskolen.no>
2026-03-07 10:07:57 +0100

    Major cleanup: remove 140 unused files (scripts, SQL, guides, dead components, debug API routes)

--- app/components/LiveVisitorsCounter.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-// This component has been removed as it was displaying simulated data
-// In the future, this could be connected to real-time analytics
-export default function LiveVisitorsCounter() {
-  // Return null to not render anything
-  return null;
-}
-
